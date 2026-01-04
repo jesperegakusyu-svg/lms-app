355d04e7245cc56880e5e902e59ca5376b97cf1c
jesperegakusyu-svg <jespere.gakusyu@gmail.com>
2026-01-04 14:16:06 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react'; // ★この1行が足りなかった！
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
-import App from './App'; // 
+import Main from './Main'; // ★ここを Main に変更
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,6 +9,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Main /> {/* ★ここも Main に変更 */}
   </React.StrictMode>
 );
