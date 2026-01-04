1b21d8fe3db25cd2729000d8e82c672a890de6e4
jesperegakusyu-svg <jespere.gakusyu@gmail.com>
2026-01-04 13:36:09 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react'; // ★この1行が足りなかった！
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import LmsApp from './LmsApp'; // ※ファイル名をLmsAppに変えた場合
+// もしファイル名が App.tsx のままなら import App from './App'; にしてください
+// 念のため、今回は前回ファイル名変更した「LmsApp」前提で書きます
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 
 root.render(
   <React.StrictMode>
