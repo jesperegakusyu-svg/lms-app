521177ca60e3921003eca3bf1e99a9ae5b57c100
jesperegakusyu-svg <jespere.gakusyu@gmail.com>
2026-01-04 14:08:12 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react'; // ★この1行が足りなかった！
 import ReactDOM from 'react-dom/client';
-import App from './App'; // ※ファイル名をAppに変えた場合
-// もしファイル名が App.tsx のままなら import App from './App'; にしてください
-// 念のため、今回は前回ファイル名変更した「App」前提で書きます
+import './style.css';
+import App from './App'; // 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
