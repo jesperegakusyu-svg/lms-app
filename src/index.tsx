3b3ff36dfc53a0e771d4c3586a249567c8b3c072
jesperegakusyu-svg <jespere.gakusyu@gmail.com>
2026-01-04 13:48:20 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'; // ★この1行が足りなかった！
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import LmsApp from './LmsApp'; // ※ファイル名をLmsAppに変えた場合
 // もしファイル名が App.tsx のままなら import App from './App'; にしてください
 // 念のため、今回は前回ファイル名変更した「LmsApp」前提で書きます
