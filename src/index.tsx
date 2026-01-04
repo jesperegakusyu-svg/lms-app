87f6a990eb0d3df09c420deae17707511f21781d
jesperegakusyu-svg <jespere.gakusyu@gmail.com>
2026-01-04 13:58:51 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'; // ★この1行が足りなかった！
 import ReactDOM from 'react-dom/client';
-import LmsApp from './LmsApp'; // ※ファイル名をLmsAppに変えた場合
+import App from './App'; // ※ファイル名をAppに変えた場合
 // もしファイル名が App.tsx のままなら import App from './App'; にしてください
-// 念のため、今回は前回ファイル名変更した「LmsApp」前提で書きます
+// 念のため、今回は前回ファイル名変更した「App」前提で書きます
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +10,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <LmsApp />
+    <App />
   </React.StrictMode>
 );
